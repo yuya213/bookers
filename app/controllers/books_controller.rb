4214dc0cc9cefff5387b84a79e29ac9b0a0a7e0f
EC2 Default User <ec2-user@ip-172-31-46-186.ap-northeast-1.compute.internal>
2023-04-15 10:00:33 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,12 +13,16 @@ class BooksController < ApplicationController
 
   def create
   	@book = Book.new(book_params)
-  	if @book.save
+  	  if @book.save
+  	  flash[:notice] = "Book was successfully updated."
   		redirect_to book_path(@book)
-  	else
-  		render :new
-  	end
+    	else
+    	 @books = Book.all
+    	 render :index 
+    	 
+    end
   end
+
 
   def edit
   	@book = Book.find(params[:id])
@@ -26,13 +30,20 @@ class BooksController < ApplicationController
 
   def update
   	@book = Book.find(params[:id])
-  	@book.update(book_params)
-    redirect_to book_path(@book.id)  
+  	if @book.update(book_params)
+  	flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id) 
+    else
+    	 @books = Book.all
+    	 render :edit
+    	 
+    end
   end
 
   def destroy
   	@book = Book.find(params[:id])
   	@book.destroy
+  	flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  
 end
