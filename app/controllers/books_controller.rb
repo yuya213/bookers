1bb5798013d9ffce52b219b2dfc20b329be93d40
EC2 Default User <ec2-user@ip-172-31-46-186.ap-northeast-1.compute.internal>
2023-04-15 07:31:09 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,43 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to book_path(@book)
+  	else
+  		render :new
+  	end
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
+    redirect_to book_path(@book.id)  
+  end
+
+  def destroy
+  	@book = Book.find(params[:id])
+  	@book.destroy
+    redirect_to '/books'
+  end
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  
+ 
   get '/top' => 'homes#top'
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books' =>'books#index'
+	post 'books' => 'books#create'
+	get 'books/:id' => 'books#show'
+	get 'books/:id/edit' => 'books#edit'
+	patch 'books/:id' => 'books#update'
+	put 'books/:id' => 'books#update'
+	delete 'books/:id' => 'books#destroy'
 end
