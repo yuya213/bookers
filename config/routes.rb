e985fb989d68c12b966e111a0df8936857d937d5
EC2 Default User <ec2-user@ip-172-31-46-186.ap-northeast-1.compute.internal>
2023-04-15 11:50:58 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
- 
-  get '/top' => 'homes#top'
+  root to: 'homes#top'
   resources :books
-  get 'books' =>'books#index'
-	post 'books' => 'books#create'
-	get 'books/:id' => 'books#show'
-	get 'books/:id/edit' => 'books#edit'
-	patch 'books/:id' => 'books#update'
-	put 'books/:id' => 'books#update'
-	delete 'books/:id' => 'books#destroy'
+  
 end
